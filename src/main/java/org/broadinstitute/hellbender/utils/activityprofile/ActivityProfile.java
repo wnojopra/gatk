fc9de8496b7dbebe3301fbd0d1e337430eb4e426
David Benjamin <davidben@broadinstitute.org>
2021-05-05 15:30:41 -0400

    some deleting without replacing yet

--- src/main/java/org/broadinstitute/hellbender/utils/activityprofile/ActivityProfile.java
+++ src/main/java/org/broadinstitute/hellbender/utils/activityprofile/ActivityProfile.java
@@ -83,13 +83,7 @@ public class ActivityProfile {
                 () -> "Adding non-contiguous state: " + loc + " after " + stopLoc());
         stateList.add(state);
     }
-
-    // --------------------------------------------------------------------------------
-    //
-    // routines to get active regions from the profile
-    //
-    // --------------------------------------------------------------------------------
-
+    
     /**
      * Get the next completed assembly regions from this profile, and remove all states supporting them from this profile
      *
@@ -119,10 +113,11 @@ public class ActivityProfile {
         final List<AssemblyRegion> regions = new ArrayList<>();
 
         // keep popping regions until we don't have enough states left to proceed and need to wait for more
+        // TODO: double-check the logic when atEndOfInterval
         while ( !(stateList.isEmpty() || (atEndOfInterval && stateList.size() < maxRegionSize) )) {
             final ActivityProfileState first = stateList.get(0);
             final boolean isActiveRegion = first.isActiveProb() > activeProbThreshold;
-            final int sizeOfNextRegion = findEndOfRegion(isActiveRegion, minRegionSize, maxRegionSize, atEndOfInterval);
+            final int sizeOfNextRegion = findEndOfRegion(isActiveRegion, minRegionSize, maxRegionSize);
             final SimpleInterval nextRegionInterval = new SimpleInterval(first.getLoc().getContig(), first.getLoc().getStart(), first.getLoc().getStart() + sizeOfNextRegion);// we need to create the active region, and clip out the states we're extracting from this profile
 
             stateList.subList(0, sizeOfNextRegion + 1).clear();
@@ -146,81 +141,20 @@ public class ActivityProfile {
      * @param isActiveRegion is the region we're looking for an active region or inactive region?
      * @param minRegionSize the minimum region size, in the case where we have to cut up regions that are too large
      * @param maxRegionSize the maximize size of the returned region
-     * @param forceConversion if true, we'll return a region whose end isn't sufficiently far from the end of the
-     *                        stateList.  Used to close out the assembly region when we've hit some kind of end (such
-     *                        as the end of the contig)
      * @return the index into stateList of the last element of this region, or -1 if it cannot be found
      */
-    private int findEndOfRegion(final boolean isActiveRegion, final int minRegionSize, final int maxRegionSize, final boolean forceConversion) {
-        int endOfActiveRegion = findFirstActivityBoundary(isActiveRegion, maxRegionSize);
+    private int findEndOfRegion(final boolean isActiveRegion, final int minRegionSize, final int maxRegionSize) {
+        // TODO: original code was endOfActiveRegion = first index of stateList where we cross probability threshold
 
         if ( isActiveRegion && endOfActiveRegion == maxRegionSize ) {
             // we've run to the end of the region, let's find a good place to cut
-            endOfActiveRegion = findBestCutSite(endOfActiveRegion, minRegionSize);
+            // TODO: original code was endOfActiveRegion = location of smoothed probability minimum
         }
 
         // we're one past the end, so i must be decremented
         return endOfActiveRegion - 1;
     }
 
-    /**
-     * Find the the local minimum within 0 - endOfActiveRegion where we should divide region
-     *
-     * This algorithm finds the global minimum probability state within the region [minRegionSize, endOfActiveRegion)
-     * (exclusive of endOfActiveRegion), and returns the state index of that state.
-     * that it
-     *
-     * @param endOfActiveRegion the last state of the current active region (exclusive)
-     * @param minRegionSize the minimum of the left-most region, after cutting
-     * @return the index of state after the cut site (just like endOfActiveRegion)
-     */
-    private int findBestCutSite(final int endOfActiveRegion, final int minRegionSize) {
-        Utils.validateArg(endOfActiveRegion >= minRegionSize, "endOfActiveRegion must be >= minRegionSize");
-        Utils.validateArg(minRegionSize >= 0, "minRegionSize must be >= 0");
-
-        int minI = endOfActiveRegion - 1;
-        double minP = Double.MAX_VALUE;
-
-        for ( int i = minI; i >= minRegionSize - 1; i-- ) {
-            double cur = getProb(i);
-            if ( cur < minP && isMinimum(i) ) {
-                minP = cur;
-                minI = i;
-            }
-        }
-
-        return minI + 1;
-    }
-
-    /**
-     * Find the first index into the state list where the state is considered ! isActiveRegion
-     *
-     * Note that each state has a probability of being active, and this function thresholds that
-     * value on activeProbThreshold, coloring each state as active or inactive.  Finds the
-     * largest contiguous stretch of states starting at the first state (index 0) with the same isActive
-     * state as isActiveRegion.  If the entire state list has the same isActive value, then returns
-     * maxRegionSize
-     *
-     * @param isActiveRegion are we looking for a stretch of active states, or inactive ones?
-     * @param maxRegionSize don't look for a boundary that would yield a region of size > maxRegionSize
-     * @return the index of the first state in the state list with isActive value != isActiveRegion, or maxRegionSize
-     *         if no such element exists
-     */
-    private int findFirstActivityBoundary(final boolean isActiveRegion, final int maxRegionSize) {
-        Utils.validateArg(maxRegionSize > 0, "maxRegionSize must be > 0");
-
-        final int nStates = stateList.size();
-        int endOfActiveRegion = 0;
-
-        while ( endOfActiveRegion < nStates && endOfActiveRegion < maxRegionSize ) {
-            if ( getProb(endOfActiveRegion) > activeProbThreshold != isActiveRegion ) {
-                break;
-            }
-            endOfActiveRegion++;
-        }
-
-        return endOfActiveRegion;
-    }
 
     /**
      * Helper function to get the probability of the state at offset index
@@ -231,31 +165,5 @@ public class ActivityProfile {
         Utils.validIndex(index, stateList.size());
 
         return stateList.get(index).isActiveProb();
-    }
-
-    /**
-     * Is the probability at index in a local minimum?
-     *
-     * Checks that the probability at index is <= both the probabilities to either side.
-     * Returns false if index is at the end or the start of the state list.
-     *
-     * @param index the index of the state we want to test
-     * @return true if prob at state is a minimum, false otherwise
-     */
-    private boolean isMinimum(final int index) {
-        Utils.validIndex(index, stateList.size());
-
-        if ( index == stateList.size() - 1 ) {
-            // we cannot be at a minimum if the current position is the last in the state list
-            return false;
-        }
-        else if ( index < 1 ) {
-            // we cannot be at a minimum if the current position is the first or second
-            return false;
-        }
-        else {
-            final double indexP = getProb(index);
-            return indexP <= getProb(index+1) && indexP < getProb(index-1);
-        }
     }
 }
